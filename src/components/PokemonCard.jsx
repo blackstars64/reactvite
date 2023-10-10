8744a2baa47b6218799408f22e7a06cbe5365d18
Blackstars <nelson.64@live.fr>
2023-10-10 10:20:15 +0200

    fini etape 2

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,12 +1,27 @@
 function PokemonCard() {
+  const pokemon = pokemonList[0];
   return (
     <figure>
-      <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-        alt="bulbizare"
-      />
-      <figcaption>Bulbizare</figcaption>
+      {pokemon.imgSrc === undefined ? (
+        <p>????</p>
+      ) : (
+        <img src={pokemon.imgSrc} alt="bulbizare" />
+      )}
+
+      <figcaption>{pokemon.name}</figcaption>
     </figure>
   );
 }
+
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 export default PokemonCard;
